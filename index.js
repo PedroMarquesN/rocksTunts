8089b124f5f1751bc87d44c7e4e8737aec274876
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-01-31 15:40:05 -0300

    feat: create function getAuthSheets

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const app = express();
 
 async function getAuthSheets(){
     const auth = new google.auth.GoogleAuth({
-        
-    })
+        keyFile:"crendials.json",
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+
+    const client = await auth.getClient();
+
+    const googleSheets = google.sheets({
+        version:"v4",
+        auth: client
+    });
+
+    const spreadsheetsId = "1XcW_098yY7Kuxbr6c2OnOxIEj0Q-9gptCMaTlNChyHA";
+    
+
+    return{
+        auth,
+        client,
+        googleSheets,
+        spreadsheetsId,
+    }
 }
