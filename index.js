8c207bd3011b5e469906faba891b155e9a3888ab
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-01-31 15:44:52 -0300

    feat: Add function update

--- index.js
+++ index.js
@@ -18,13 +18,53 @@ async function getAuthSheets(){
         auth: client
     });
 
-    const spreadsheetsId = "1XcW_098yY7Kuxbr6c2OnOxIEj0Q-9gptCMaTlNChyHA";
-    
+    const spreadsheetId = "1XcW_098yY7Kuxbr6c2OnOxIEj0Q-9gptCMaTlNChyHA";
+    const classesRange = "engenharia_de_software!A2";
 
+    const classesData = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: classesRange,
+      });
+
+
+    const classes = parseInt(classesData.data.values[0][0]);
+
+    
     return{
         auth,
         client,
         googleSheets,
-        spreadsheetsId,
+        spreadsheetId,
     }
 }
+
+
+async function updateResults(auth, spreadsheetId, row, situation, naf) {
+    const sheets = google.sheets({ version: "v4", auth });
+  
+    const range = `engenharia_de_software!G${row + 4}:H${row + 4}`;
+    const values = [[situation, naf]];
+  
+    const resource = {
+      values,
+    };
+  
+    try {
+      await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range,
+        valueInputOption: "USER_ENTERED",
+        resource,
+      });
+  
+    } catch (err) {
+      console.error(`Error updating results for row ${row + 4}:`, err);
+    }
+  }
+
+
+
+
+
+app.listen(3001, ()=> console.log("Rodando na porta 3001"))
